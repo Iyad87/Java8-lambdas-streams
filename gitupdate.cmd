491787e751130220f2d5edd409f5af972c0e80e8
dalonsogz <dalonsogz@yahoo.es>
2018-09-22 18:06:40 +0200

    20180922180608 commit

--- gitupdate.cmd
+++ gitupdate.cmd
@@ -1,6 +1,6 @@
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%
 echo %mydate%
 git add .
-git commit -m '%mydate% commit'
+git commit -m "%mydate% commit"
 git push -u Java8-lambdas-streams master
 pause
