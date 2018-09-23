c6d811b05b9425430367d2240f63ef33282acf51
dalonsogz <dalonsogz@yahoo.es>
2018-09-23 15:53:21 +0200

    20180923155317 commit

--- 1_01_problema/src/org/formacion/Problema.java
+++ 1_01_problema/src/org/formacion/Problema.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Problema {
 
@@ -11,10 +13,13 @@ public class Problema {
 
 		List<String> nombres = Arrays.asList("Juan", "Antonia", "Pedro");
 
-		Comparator<String> comparadorLongitud = (o1, o2) -> o1.length() - o2.length();
-		Collections.sort(nombres, comparadorLongitud);
+//		Comparator<String> comparadorLongitud = (o1, o2) -> o1.length() - o2.length();
+//		Collections.sort(nombres, comparadorLongitud);
 
-		System.out.println(nombres);
+		
+		List<String> nombresOrdered = nombres.stream().sorted((o1, o2) -> o1.length() - o2.length()).collect(Collectors.toList());
+		
+		System.out.println(nombresOrdered);
 	}
 
 }
--- 2_02_lambda_expressions/src/org/formacion/LambdasFactory.java
+++ 2_02_lambda_expressions/src/org/formacion/LambdasFactory.java
@@ -25,28 +25,22 @@ public class LambdasFactory {
 	
 	/** Devuelve una implemtacion de constante que siempre devuelve 5 */
 	public Constante devuelve5() {
-		return () -> 5;
+		return null;
 	}
 
 	/** Devuelve una implementacion de Operador con el metodo de suma de dos enteros */
 	public Operador obtenSumador() {
-		return (a,b) -> a+b;
+		return null;
 	}
 	
 	/** 
 	 * Devuelve un inicializador de un array de strings
 	 * El metodo inicializa recibe el array ya creado. Su tarea es que 
 	 * todas las posiciones queden inicializadas con el valor indicado en el
-	 * segundo parametro del metodo incializa
+	 * seguno parametro del metodo incializa
 	 */
 	public InicializadorArrays obtenInicializador() {
-		// implementa void inicializa ( String[] array, String valor) 
-		return (array, valor) -> {
-			// usamos un bloque en lugar de una expresion
-			for (int i = 0; i < array.length; i++) {
-				array[i] = valor;
-			}
-		};
+		return null;
 	}
 	
 }
